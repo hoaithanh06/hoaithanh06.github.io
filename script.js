0820d6dd06a29621ca1645eed5ac1b0a24ad2994
hoaithanh06 <lysonhoaithanhfc@gmail.com>
2026-01-17 15:18:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const music = document.getElementById("bgMusic");
 const container = document.querySelector(".container");
 
 btn.onclick = () => {
-  music.volume = 0.6;
   music.play();
-
   overlay.style.display = "none";
   container.classList.remove("hidden");
 };
